e72d4612693a934e7b2c5b386b0ff0b2668cb2cc
John Lindquist <johnlindquist@gmail.com>
2021-10-23 08:33:12 -0600

    fix(ts): split into multiple declaration files

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,53 +1,3 @@
-//axios
-declare var get: import("./axios").AxiosInstance["get"]
-declare var put: import("./axios").AxiosInstance["put"]
-declare var post: import("./axios").AxiosInstance["post"]
-declare var patch: import("./axios").AxiosInstance["patch"]
-//chalk
-declare var chalk: typeof import("./chalk")
-//child_process
-declare var spawn: typeof import("child_process").spawn
-declare var spawnSync: typeof import("child_process").spawnSync
-declare var fork: typeof import("child_process").fork
-declare var degit: typeof import("./degit")
-//download
-declare var download: typeof import("./download")
-//fs-extra
-declare var emptyDir: typeof import("./fs-extra").emptyDir
-declare var ensureFile: typeof import("./fs-extra").ensureFile
-declare var ensureDir: typeof import("./fs-extra").ensureDir
-declare var ensureLink: typeof import("./fs-extra").ensureLink
-declare var ensureSymlink: typeof import("./fs-extra").ensureSymlink
-declare var mkdirp: typeof import("./fs-extra").mkdirp
-declare var mkdirs: typeof import("./fs-extra").mkdirs
-declare var outputFile: typeof import("./fs-extra").outputFile
-declare var outputJson: typeof import("./fs-extra").outputJson
-declare var pathExists: typeof import("./fs-extra").pathExists
-declare var readJson: typeof import("./fs-extra").readJson
-declare var remove: typeof import("./fs-extra").remove
-declare var writeJson: typeof import("./fs-extra").writeJson
-//fs/promises
-declare var readFile: typeof import("fs/promises").readFile
-declare var writeFile: typeof import("fs/promises").writeFile
-declare var appendFile: typeof import("fs/promises").appendFile
-declare var readdir: typeof import("fs/promises").readdir
-declare var copyFile: typeof import("fs/promises").copyFile
-//fs
-declare var createReadStream: typeof import("fs").createReadStream
-declare var createWriteStream: typeof import("fs").createWriteStream
-//handlebars
-declare var compile: typeof import("./handlebars").compile
-//lodash
-declare var _: import("./lodash").LoDashStatic
-//marked
-declare var md: typeof import("./marked").parse
-//nonoid
-declare var uuid: typeof import("./nanoid").nanoid
-//node-fetch
-declare var fetch: typeof import("./node-fetch")
-//replace-in-file
-declare var replace: typeof import("./replace-in-file").replaceInFile
-//zx
 import { ChildProcess } from "child_process"
 import { Readable, Writable } from "stream"
 
@@ -73,7 +23,7 @@ interface ProcessPromise<T> extends Promise<T> {
   kill(signal?: string | number): Promise<void>
 }
 
-class ProcessOutput {
+declare class ProcessOutput {
   readonly exitCode: number
   readonly stdout: string
   readonly stderr: string
@@ -81,4 +31,98 @@ class ProcessOutput {
   toString(): string
 }
 
-declare var $: $
+declare global {
+  //axios
+  var get: import("./axios").AxiosInstance["get"]
+  var put: import("./axios").AxiosInstance["put"]
+  var post: import("./axios").AxiosInstance["post"]
+  var patch: import("./axios").AxiosInstance["patch"]
+  //chalk
+  var chalk: typeof import("./chalk")
+  //child_process
+  var spawn: typeof import("child_process").spawn
+  var spawnSync: typeof import("child_process").spawnSync
+  var fork: typeof import("child_process").fork
+  var degit: typeof import("./degit")
+  //download
+  var download: typeof import("./download")
+  //fs-extra
+  var emptyDir: typeof import("./fs-extra").emptyDir
+  var ensureFile: typeof import("./fs-extra").ensureFile
+  var ensureDir: typeof import("./fs-extra").ensureDir
+  var ensureLink: typeof import("./fs-extra").ensureLink
+  var ensureSymlink: typeof import("./fs-extra").ensureSymlink
+  var mkdirp: typeof import("./fs-extra").mkdirp
+  var mkdirs: typeof import("./fs-extra").mkdirs
+  var outputFile: typeof import("./fs-extra").outputFile
+  var outputJson: typeof import("./fs-extra").outputJson
+  var pathExists: typeof import("./fs-extra").pathExists
+  var readJson: typeof import("./fs-extra").readJson
+  var remove: typeof import("./fs-extra").remove
+  var writeJson: typeof import("./fs-extra").writeJson
+  //fs/promises
+  var readFile: typeof import("fs/promises").readFile
+  var writeFile: typeof import("fs/promises").writeFile
+  var appendFile: typeof import("fs/promises").appendFile
+  var readdir: typeof import("fs/promises").readdir
+  var copyFile: typeof import("fs/promises").copyFile
+  //fs
+  var createReadStream: typeof import("fs").createReadStream
+  var createWriteStream: typeof import("fs").createWriteStream
+  //handlebars
+  var compile: typeof import("./handlebars").compile
+  //lodash
+  var _: import("./lodash").LoDashStatic
+  //marked
+  var md: typeof import("./marked").parse
+  //nonoid
+  var uuid: typeof import("./nanoid").nanoid
+  //node-fetch
+  var fetch: typeof import("./node-fetch")
+  //replace-in-file
+  var replace: typeof import("./replace-in-file").replaceInFile
+  //zx
+  var $: $
+
+  namespace NodeJS {
+    interface Global {
+      get: import("./axios").AxiosInstance["get"]
+      put: import("./axios").AxiosInstance["put"]
+      post: import("./axios").AxiosInstance["post"]
+      patch: import("./axios").AxiosInstance["patch"]
+      chalk: typeof import("./chalk")
+      spawn: typeof import("child_process").spawn
+      spawnSync: typeof import("child_process").spawnSync
+      fork: typeof import("child_process").fork
+      degit: typeof import("./degit")
+      download: typeof import("./download")
+      emptyDir: typeof import("./fs-extra").emptyDir
+      ensureFile: typeof import("./fs-extra").ensureFile
+      ensureDir: typeof import("./fs-extra").ensureDir
+      ensureLink: typeof import("./fs-extra").ensureLink
+      ensureSymlink: typeof import("./fs-extra").ensureSymlink
+      mkdirp: typeof import("./fs-extra").mkdirp
+      mkdirs: typeof import("./fs-extra").mkdirs
+      outputFile: typeof import("./fs-extra").outputFile
+      outputJson: typeof import("./fs-extra").outputJson
+      pathExists: typeof import("./fs-extra").pathExists
+      readJson: typeof import("./fs-extra").readJson
+      remove: typeof import("./fs-extra").remove
+      writeJson: typeof import("./fs-extra").writeJson
+      readFile: typeof import("fs/promises").readFile
+      writeFile: typeof import("fs/promises").writeFile
+      appendFile: typeof import("fs/promises").appendFile
+      readdir: typeof import("fs/promises").readdir
+      copyFile: typeof import("fs/promises").copyFile
+      createReadStream: typeof import("fs").createReadStream
+      createWriteStream: typeof import("fs").createWriteStream
+      compile: typeof import("./handlebars").compile
+      _: import("./lodash").LoDashStatic
+      md: typeof import("./marked").parse
+      uuid: typeof import("./nanoid").nanoid
+      fetch: typeof import("./node-fetch")
+      replace: typeof import("./replace-in-file").replaceInFile
+      $: $
+    }
+  }
+}
