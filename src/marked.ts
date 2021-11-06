2c8281cab206c916d170b79208d8b6217dc9dcde
John Lindquist <johnlindquist@gmail.com>
2021-11-06 11:40:47 -0600

    fix(md): add default containerClasses

--- src/marked.ts
+++ src/marked.ts
@@ -1,7 +1,7 @@
 import _marked from "marked"
 
 export let marked = (global.marked = _marked)
-export let md = (global.md = (markdown: string, containerClasses?: string) => {
+export let md = (global.md = (markdown: string, containerClasses: string = `p-5`) => {
   let html = _marked.parse(markdown)
   if (containerClasses) {
     return `<div class="${containerClasses}">${html}</div>`
