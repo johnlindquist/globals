711611272dcade0797bb29e61f532fe5d7fe11f2
John Lindquist <johnlindquist@gmail.com>
2021-11-11 10:48:27 -0700

    fix(md): adding prose to md

--- src/marked.ts
+++ src/marked.ts
@@ -1,7 +1,7 @@
 import _marked from "marked"
 
 export let marked = (global.marked = _marked)
-export let md = (global.md = (markdown: string, containerClasses: string = `p-5`) => {
+export let md = (global.md = (markdown: string, containerClasses: string = `p-5 prose dark:prose-dark`) => {
   let html = _marked.parse(markdown)
   if (containerClasses) {
     return `<div class="${containerClasses}">${html}</div>`
