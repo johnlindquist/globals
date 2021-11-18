664aaee077169537abd8cec356cbdec3a6828d9f
John Lindquist <johnlindquist@gmail.com>
2021-11-18 10:09:40 -0700

    feat(md): adding marked-forms

--- src/marked.ts
+++ src/marked.ts
@@ -1,4 +1,7 @@
 import _marked from "marked"
+import _markedForms from "marked-forms"
+
+_marked.use(_markedForms())
 
 export let marked = (global.marked = _marked)
 export let md = (global.md = (markdown: string, containerClasses: string = `p-5 prose dark:prose-dark`) => {
